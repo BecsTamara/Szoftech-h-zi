1b22655281438b8f3455b02a19f4b43267f49ccc
Konkoly Dominik <90547972+KonkolyDominik@users.noreply.github.com>
2025-12-07 20:56:49 +0100

    .csv , .xml formátum működik

--- Szoftech-H-zi/Services/Importalas/CSVImportalo.cs
+++ Szoftech-H-zi/Services/Importalas/CSVImportalo.cs
@@ -3,18 +3,14 @@ using MeterologiaiAdatbazis.Model;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Linq;
 
 namespace MeterologiaiAdatbazis.Services.Importalas
 {
     public class CsvImportalo : IImportalo
     {
         public string FajlNev => "CSV Importalo";
-
-        public CsvImportalo() { }
 
         public (int sikeres, int hibas, List<Adat> lista) Importal(string fajl)
         {
@@ -25,25 +21,34 @@ namespace MeterologiaiAdatbazis.Services.Importalas
             if (!File.Exists(fajl))
                 throw new FileNotFoundException("A megadott CSV fájl nem található.", fajl);
 
-            // A kiírás szerint a hibás sorok nem okozhatnak programleállást
-            foreach (var sor in File.ReadAllLines(fajl).Skip(0))
+            // Skip(1)-et használunk, feltételezve, hogy van fejléc.
+            // Ha a fájljokban NINCS fejléc, írd vissza Skip(0)-ra!
+            foreach (var sor in File.ReadAllLines(fajl).Skip(1))
             {
+                // Üres sorok átugrása
+                if (string.IsNullOrWhiteSpace(sor)) continue;
+
                 var mezok = sor.Split(';');
+
+                // Minimum 3 mező kell (Idő, Érték, Egység)
                 if (mezok.Length < 3)
                 {
                     hibas++;
                     continue;
                 }
 
-                // Használjuk az InvariantCulture-t, hogy biztosítsuk a tizedes ponttal való beolvasást
+                // InvariantCulture a tizedespontos számokhoz (pl. 12.5)
                 if (DateTime.TryParse(mezok[0], out DateTime ido) &&
                     double.TryParse(mezok[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double ertek))
                 {
-                    lista.Add(new Adat(ido, ertek, mezok[2], AdatEredet.Imported)
+                    var adat = new Adat(ido, ertek, mezok[2], AdatEredet.Imported)
                     {
-                        SzenzorNev = mezok.Length > 3 ? mezok[3] : null,
-                        Kategoria = mezok.Length > 4 ? mezok[4] : null
-                    });
+                        // Opcionális mezők kezelése (Szenzor és Kategória)
+                        SzenzorNev = mezok.Length > 3 && !string.IsNullOrWhiteSpace(mezok[3]) ? mezok[3] : null,
+                        Kategoria = mezok.Length > 4 && !string.IsNullOrWhiteSpace(mezok[4]) ? mezok[4] : null
+                    };
+
+                    lista.Add(adat);
                     sikeres++;
                 }
                 else
--- Szoftech-H-zi/obj/Release/net8.0/MeterologiaiAdatbazis.AssemblyInfo.cs
+++ Szoftech-H-zi/obj/Release/net8.0/MeterologiaiAdatbazis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeterologiaiAdatbazis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b3167a75c12bc0fd5711e092be2fd94fdefe666")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeterologiaiAdatbazis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeterologiaiAdatbazis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
